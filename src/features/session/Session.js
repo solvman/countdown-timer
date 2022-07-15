c9e04a46fe4bf6e8ff2cf505c9d0250e62af7be8
solvman <solvman@gmail.com>
2022-07-14 22:02:40 -0500

    add perfect second functionality

--- src/features/session/Session.js
+++ src/features/session/Session.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, setTimerOn } from "./sessionSlice";
+import { decrement, setTimerOn, setTimerType, setTimer } from "./sessionSlice";
 import { setDefault as setDefaultBreakLength } from "../breaklength/breakLengthSlice";
 import { setDefault as setDefaultSessionLength } from "../sessionlength/sessionLengthSlice";
 import { setDefault as setDefaultSession } from "../session/sessionSlice";
@@ -13,14 +13,37 @@ import {
 
 function Session() {
   const timer = useSelector((state) => state.session.timer);
+  const timerType = useSelector((state) => state.session.type);
   const timerOn = useSelector((state) => state.session.timerOn);
+  const breakLength = useSelector((state) => state.break.value);
+  const sessionLength = useSelector((state) => state.sessionlength.value);
   const dispatch = useDispatch();
 
   useEffect(() => {
     let timerId = null;
+    let second = 1000;
+    let date = new Date().getTime();
+    let nextDate = date + second;
 
     if (timerOn) {
-      timerId = setInterval(() => dispatch(decrement()), 1000);
+      timerId = setInterval(() => {
+        date = new Date().getTime();
+        if (date > nextDate) {
+          dispatch(decrement());
+          nextDate += second;
+        }
+
+        if (timer === 0) {
+          console.log("Beeeep");
+          if (timerType === "session") {
+            dispatch(setTimer(breakLength * 60));
+            dispatch(setTimerType("break"));
+          } else if (timerType === "break") {
+            dispatch(setTimer(sessionLength * 60));
+            dispatch(setTimerType("session"));
+          }
+        }
+      }, 10);
     } else {
       clearInterval(timerId);
     }
@@ -28,7 +51,8 @@ function Session() {
     return () => {
       clearInterval(timerId);
     };
-  }, [timerOn, dispatch]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [timerOn, dispatch, timer]);
 
   const ResetTimer = () => {
     dispatch(setDefaultSession());
@@ -38,7 +62,7 @@ function Session() {
 
   return (
     <div className="session">
-      <h3>Session</h3>
+      <h3 className="timer-type">{timerType}</h3>
       <h1 className="timer">
         <span>{("0" + Math.floor(timer / 60)).slice(-2)}</span>
         <span>:</span>
--- src/features/session/sessionSlice.js
+++ src/features/session/sessionSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  type: "session",
   timer: 25 * 60,
   timerOn: false,
 };
@@ -16,14 +17,19 @@ export const sessionSlice = createSlice({
       state.timerOn = action.payload;
     },
     setTimer: (state, action) => {
-      state.timer = action.payload;
+      if (action.payload >= 60 && action.payload <= 60 * 60) {
+        state.timer = action.payload;
+      }
     },
     setDefault: (state) => {
       state.timer = initialState.timer;
+    },
+    setTimerType: (state, action) => {
+      state.type = action.payload;
     },
   },
 });
 
 export default sessionSlice.reducer;
-export const { decrement, setTimerOn, setTimer, setDefault } =
+export const { decrement, setTimerOn, setTimer, setDefault, setTimerType } =
   sessionSlice.actions;
